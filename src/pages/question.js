6242f3e0c8cc91ebba12d0fc9e403eeee53494bc
leodder <a0919989772@gmail.com>
2023-09-11 10:47:18 +0800

    911

---
+++ src/pages/question.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//component
+import AlternatingSumCalculator from '/components/q1.js'
+
+export default function question() {
+  return (
+    <div>
+    <AlternatingSumCalculator />
+    </div>
+  )
+}
